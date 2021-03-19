db97210e6d2db665d4de50744ed47c8d64c57907
Aleksey Kulgeyko <kulgeyko93@mail.ru>
2021-03-19 21:46:34 +0300

    feat: basic sturctures

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = (): JSX.Element => (
+  <div className="main">
+    <NavLink to="/">MAIN pages</NavLink>
+  </div>
+);
+
+export default Header;

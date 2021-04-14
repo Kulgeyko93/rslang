872c132dab4f91c162a13c2bdde7a2218a281ae4
burhonov <38163059+Burhonov@users.noreply.github.com>
2021-04-14 16:06:12 +0500

    feat: sign out

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { NavLink } from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Image from 'react-bootstrap/Image';
-import { BoxArrowInRight, BoxArrowLeft } from 'react-bootstrap-icons';
+import { Container, Col, Image, OverlayTrigger, Popover, Row, Button } from 'react-bootstrap';
+import { BoxArrowInRight, PersonCheck, ArrowRepeat } from 'react-bootstrap-icons';
+import axios from 'axios';
 import logoImg from '../../assets/icons/logo.svg';
 import menuImg from '../../assets/icons/menu.svg';
 import settingsImg from '../../assets/icons/settings.svg';
 import styles from './header.module.css';
-import { selectAuthData } from '../../features/auth/authSlice';
+import { selectAuthData, selectAuthStatus, setAuthData } from '../../features/auth/authSlice';
+import InlineSpinner from '../InlineSpinner';
+import { Status, StorageKey } from '../../types';
 
 const imgStyle = {
   marginRight: '30px',
@@ -20,12 +20,72 @@ interface Props {
   openAuthModal: () => void;
 }
 
+interface UserData {
+  email: string;
+  photo_url: string;
+}
+
 const Header = (props: Props): JSX.Element => {
   const { openAuthModal } = props;
+  const dispatch = useDispatch();
+  const [userData, setUserData] = useState<UserData | null>(null);
+  const [isLoggingOut, setIsLoggingOut] = useState(false);
+  const authStatus = useSelector(selectAuthStatus);
   const authData = useSelector(selectAuthData);
 
-  const signOutButton = <BoxArrowLeft size={20} />;
-  const signInButton = <BoxArrowInRight size={20} onClick={openAuthModal} />;
+  async function fetchAndSetUserData() {
+    const userDataFromServer = await axios.get(`/users/${authData?.userId}`);
+    if (!userDataFromServer?.data) {
+      throw new Error('Недействительный пользователь');
+    }
+    setUserData(userDataFromServer.data);
+  }
+
+  function logoutUser() {
+    setIsLoggingOut(true);
+    dispatch(setAuthData(null));
+    localStorage.removeItem(StorageKey.AuthTime);
+    localStorage.removeItem(StorageKey.Auth);
+    setTimeout(() => setIsLoggingOut(false), 3000);
+  }
+
+  let authButton;
+  if (userData) {
+    const { email, photo_url: photoUrl } = userData;
+    const popover = (
+      <Popover id="popover-basic">
+        <Popover.Title as="h3">{email}</Popover.Title>
+        <Popover.Content>
+          <Image className="mb-2" width={200} src={photoUrl} rounded />
+          <Button disabled={isLoggingOut} onClick={logoutUser} variant="outline-info">
+            {isLoggingOut && <InlineSpinner />}
+            &nbsp; Разавторизовать
+          </Button>
+        </Popover.Content>
+      </Popover>
+    );
+    authButton = (
+      <OverlayTrigger trigger="click" placement="left" overlay={popover}>
+        <PersonCheck size={20} />
+      </OverlayTrigger>
+    );
+  } else if (authData) {
+    authButton = <ArrowRepeat size={20} />;
+  } else {
+    authButton = <BoxArrowInRight size={20} onClick={openAuthModal} />;
+  }
+
+  useEffect(() => {
+    if (authStatus === Status.Authorized && authData?.userId) {
+      try {
+        fetchAndSetUserData();
+      } catch (e) {
+        setUserData(null);
+      }
+    } else {
+      setUserData(null);
+    }
+  }, [authStatus, authData]);
 
   return (
     <div className={styles.container}>
@@ -40,7 +100,7 @@ const Header = (props: Props): JSX.Element => {
           </Col>
           <Col className={styles.right}>
             <div style={{ ...imgStyle, width: 20, height: 'auto' }} className={styles.img}>
-              {authData ? signOutButton : signInButton}
+              {authButton}
             </div>
             <div style={imgStyle} className={styles.img}>
               <Image width="20" height="auto" src={settingsImg} fluid />
